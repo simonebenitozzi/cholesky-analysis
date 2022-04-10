65ce07932c0abb5b7ddb114941ded5a53cecb483
Mario Avolio <marioavolio1999@gmail.com>
2022-04-10 21:34:25 +0200

    upgrade python script

--- Python/Analysis.py
+++ Python/Analysis.py
@@ -1,3 +1,9 @@
 import numpy as np
+from scipy.linalg import cholesky
+from scipy.io import mmread
 
-# TODO
+matrix = mmread('Matrix/apache2.mtx').toarray()
+print(type(matrix))
+# print(matrix)
+# L = cholesky(matrix, lower=True) # np.linalg.cholesky(A)
+# print(L)
