12c13fabcd55d86c0098b53cfc974a5bec9e550f
Mario Avolio <marioavolio1999@gmail.com>
2022-04-11 15:15:26 +0200

    upgrade

--- Python/Analysis.py
+++ Python/Analysis.py
@@ -1,9 +1,11 @@
 import numpy as np
 from scipy.linalg import cholesky
 from scipy.io import mmread
+from scipy import sparse
 
-matrix = mmread('Matrix/apache2.mtx').toarray()
-print(type(matrix))
-# print(matrix)
-# L = cholesky(matrix, lower=True) # np.linalg.cholesky(A)
-# print(L)
+matrix = sparse.csr_matrix(mmread('Matrix/apache2.mtx'))
+print(f"\n Matrix Type: {type(matrix)} \n")
+print(f"Matrix: \n ------------------- \n {matrix} \n ---------------- \n")
+# L = cholesky(matrix, lower=True)
+L = np.linalg.cholesky(matrix)
+print(L)
