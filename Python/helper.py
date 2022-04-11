4b0d430b1d764d50862885d8e9957fe4f9d8ef76
Mario Avolio <marioavolio1999@gmail.com>
2022-04-11 16:21:34 +0200

    change python project structure

--- Python/helper.py
+++ Python/helper.py
@@ -1,16 +1,6 @@
-from scipy.sparse import linalg as splinalg
-import scipy.sparse as sparse
-import sys
+from scipy.io import mmread
 
 
-# The input matrix A must be a sparse symmetric positive-definite.
-def sparse_cholesky(A):
-
-    n = A.shape[0]
-    LU = splinalg.splu(A, diag_pivot_thresh=0)  # sparse LU decomposition
-
-    # check the matrix A is positive definite.
-    if (LU.perm_r == np.arange(n)).all() and (LU.U.diagonal() > 0).all():
-        return LU.L.dot(sparse.diags(LU.U.diagonal()**0.5))
-    else:
-        sys.exit('The matrix is not positive definite')
+def readMatrix(filename):
+    matrix = mmread(f'Matrix/{filename}.mtx')
+    return matrix
