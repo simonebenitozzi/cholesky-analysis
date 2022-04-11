18dab25b48fd785f10485d01594bd75096896fe9
Mario Avolio <marioavolio1999@gmail.com>
2022-04-11 17:32:00 +0200

    insert sparse_cholesky

--- Python/helper.py
+++ Python/helper.py
@@ -1,5 +1,31 @@
+import numpy as np
+from scipy.sparse import linalg as splinalg
+import sys
+import scipy.sparse as sparse
 from scipy.io import mmread
 
 
 def readMatrix(filename):
     return mmread(f'Matrix/{filename}.mtx')
+
+
+# The input matrix A must be a sparse symmetric positive-definite.
+def sparse_cholesky(A):
+    """ 
+    Scipy does not currently provide a routine for cholesky decomposition of a sparse matrix, 
+    and one have to rely on another external package such as scikit.sparse for the purpose.
+    Here I implement cholesky decomposition of a sparse matrix only using scipy functions. 
+    Our implementation relies on sparse LU deconposition.
+    The following function receives a sparse symmetric positive-definite matrix A 
+    and returns a spase lower triangular matrix L such that A = LL^T.
+    
+    """
+
+    n = A.shape[0]
+    LU = splinalg.splu(A, diag_pivot_thresh=0)  # sparse LU decomposition
+
+    # check the matrix A is positive definite.
+    if (LU.perm_r == np.arange(n)).all() and (LU.U.diagonal() > 0).all():
+        return LU.L.dot(sparse.diags(LU.U.diagonal()**0.5))
+    else:
+        sys.exit('The matrix is not positive definite')
