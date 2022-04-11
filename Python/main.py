95d461478405bcadafdf6f639e18516304babb45
Mario Avolio <marioavolio1999@gmail.com>
2022-04-11 19:02:50 +0200

    upgrade

--- Python/main.py
+++ Python/main.py
@@ -2,20 +2,21 @@ from posixpath import dirname
 from scipy import sparse
 from scipy.io import mmread
 from scipy.linalg import cholesky
+from scipy.sparse import csc_matrix
 import numpy as np
 import sys
-from sksparse.cholmod import cholesky
 
 sys.path.append(dirname(__file__))
 
-from Python.helper import readMatrix
+from Python.helper import readMatrix, sparse_cholesky
 
 
 
 
-matrix = readMatrix("apache2")
+matrix = csc_matrix(readMatrix("apache2"))
 print(f"\n Matrix Type: {type(matrix)} \n")
 print(f"Matrix: \n ------------------- \n {matrix} \n ---------------- \n")
+L = sparse_cholesky(matrix)
 # L = cholesky(matrix, lower=True)
 # L = np.linalg.cholesky(matrix)
-# print(L)
+print(L)
