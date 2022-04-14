49a4cfb2886142c86bd5f1f11d1e30c743b89020
Simone Benitozzi <72170256+simonebenitozzi@users.noreply.github.com>
2022-04-14 15:52:03 +0200

    Matlab Cholesky iteration

--- MATLAB/cholesky_decomposition.m
+++ MATLAB/cholesky_decomposition.m
@@ -1,39 +1,68 @@
 clear all, close all, clc
 
-% --- matrix loading
-folder_path = '';
-matrix_name = 'ex15.mat';
-matrix_path = strcat(folder_path, matrix_name);
-load(matrix_path)
-A = Problem.A;
-% spy(A)
-clear Problem matrix_path folder_path matrix_name
+current_path = pwd;
+files = dir(strcat(current_path, '\matrix\*.mat'));
+N = length(files);
 
-% memory usage before execution (after loading matrix)
-before_mem = memory;
+% data preallocation
+data = strings(N+1, 4);
+data(1,:) = ["name", "error", "memory", "time"];
 
-% problem parameters
-xe = ones(size(A, 1), 1);
-b = A*xe;
+for i=1:N
+    fprintf("-----------------------------------\n");
+    
+    % --- matrix loading
+    folder_path = files(i).folder;
+    matrix_name = files(i).name;
+    matrix_path = strcat(folder_path, '\', matrix_name);
+    load(matrix_path)
+    A = Problem.A;
+    % spy(A)
+    
+    [~, matrix_name, ~] = fileparts(matrix_name);
+    fprintf("Matrix: %s\n", matrix_name);
+    clear Problem matrix_path folder_path
 
-% --- system solving
+    % memory usage before execution (after loading matrix)
+    before_mem = memory;
 
-tic %starts timing
-% Cholesky decomposition
-R = chol(A);
+    % problem parameters
+    xe = ones(size(A, 1), 1);
+    b = A*xe;
 
-% solution computing
-x = R\(R'\b);
+    % --- system solving
 
-time = toc; % ends timing
-fprintf("Time elapsed: %f seconds\n", time)
+    try
+        tic %starts timing
+        % Cholesky decomposition
 
-% --- memory usage estimation
-after_mem = memory;
-mem_difference = after_mem.MemUsedMATLAB-before_mem.MemUsedMATLAB;
-mem_difference_mb = mem_difference*1e-6;
-fprintf("\nTotal memory used: %e bytes (%f MB)\n", mem_difference, mem_difference_mb)
+        R = chol(A);
 
-% --- error estimation
-err = norm(x - xe, 2) / norm(xe, 2);
-fprintf("\nRelative error: %e\nepsilon: %e\n", err, eps(1))
+        % solution computing
+        x = R\(R'\b);
+
+        time = toc; % ends timing
+        fprintf("Time elapsed: %f seconds\n", time)
+
+    catch exception
+        fprintf("Error: %s\n", exception.identifier)
+        
+        data(i+1,:) = [matrix_name, "", "", ""];
+        continue
+    end
+    
+    % --- error estimation
+    err = norm(x - xe, 2) / norm(xe, 2);
+    fprintf("\nRelative error: %e\nepsilon: %e\n", err, eps(1))
+
+    % --- memory usage estimation
+    after_mem = memory;
+    mem_difference = after_mem.MemUsedMATLAB-before_mem.MemUsedMATLAB;
+    mem_difference_mb = mem_difference*1e-6;
+    fprintf("\nTotal memory used by MATLAB: %e bytes (%f MB)\n", mem_difference, mem_difference_mb)
+    fprintf("-----------------------------------\n\n\n");
+    
+    data(i+1, :) = [matrix_name, string(err), string(mem_difference_mb), string(time)];
+end
+
+writematrix(data, 'matlab.csv')
