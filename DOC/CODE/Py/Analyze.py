5a88bd5883721e9f558cc85225d49ae1c53e4961
Mario Avolio <marioavolio1999@gmail.com>
2022-04-15 17:36:42 +0200

    upgrade DOC

---
+++ DOC/CODE/Py/Analyze.py
@@ -0,0 +1,48 @@
+class Analyze:
+    def __init__(self):
+        self.__name = None
+        self.__error = None
+        self.__memoryUsed = None
+        self.__timeTotal = None
+        writeCSV('Name', 'Error', 'Memory', 'Time',
+                 language='Language', operatingSystem='OS')  # write header
+
+    def __analyze(self, path):
+
+        matrix = readMatrix(path)  # read matrix
+        b = getB(matrix)  # get b = A*xe
+
+        # start time and memory track
+        start_time = time.time()
+        startTrackMemory()
+        ###########################
+
+        # CHOLESKY
+        x = scikit_sparse_cholesky(matrix, b)
+        ##########################
+
+        # stop time and memory track
+        self.__memoryUsed = convert_size(endTrackMemory())
+        self.__timeTotal = time.time() - start_time
+        ##########################
+
+        # compute distance
+        self.__error = relativeError(x)
+
+    def __setName(self, name):
+        self.__name = name.split(".mtx")[0]
+
+    def startAnalyze(self, path, name):
+        self.__setName(name)
+
+        # Analysis
+        self.__analyze(path)
+
+        # write data
+        writeCSV(self.__name, self.__error,
+                 self.__memoryUsed, self.__timeTotal, language=1, operatingSystem=getOperatingSystem())
+
+        print(f"Name: {self.__name}")
+        print(f"Memory: {self.__memoryUsed}")
+        print(f"Seconds: {self.__timeTotal} \n")
+        print(f"Error: {self.__error}")
