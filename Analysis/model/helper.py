42e2df8ab4600467a88a7559648765c471facc65
simone <simonebenitozzi@gmail.com>
2022-04-18 09:48:42 +0200

    helper.py update: write_data

--- Analysis/model/helper.py
+++ Analysis/model/helper.py
@@ -1,3 +1,4 @@
+from operator import index
 import os
 
 import pandas as pd
@@ -7,3 +8,8 @@ def read_data(name):
     from Analysis.resources.costants import RESOURCES_DIRECTORY
     path = os.path.join(RESOURCES_DIRECTORY, name)
     return pd.read_csv(path)
+
+def write_data(df, name):
+    from Analysis.resources.costants import RESOURCES_DIRECTORY
+    path = os.path.join(RESOURCES_DIRECTORY, name)
+    df.to_csv(path, index=False)
